7c2ed72cba0e7d625f397bfccb3cace25cfadc96
蒔苗ヒナ <hina@shimiaohinade-MacBook-Pro.local>
2017-04-14 12:34:43 +0900

    implement part 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,10 @@
-class Admin::JobsController < ApplicationController
+class ApplicationController < ActionController::Base
+
+
   def require_is_admin
     if !current_user.admin?
       flash[:alert] = 'You are not admin'
       redirect_to root_path
     end
   end
-end  
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170413084830) do
+ActiveRecord::Schema.define(version: 20170414010858) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
