8bf9f7b6bf608f3bb4e09fea52e70ea2fc47ca31
蒔苗ヒナ <hina@shimiaohinade-MacBook-Pro.local>
2017-04-17 12:47:23 +0900

    implement step 5

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,11 @@ Rails.application.routes.draw do
    root 'jobs#index'
 
 
-namespace :admin do
-    resources :jobs
-  end
-end
+   namespace :admin do
+       resources :jobs do
+         member do
+           post :publish
+           post :hide
+         end
+       end
+     end
